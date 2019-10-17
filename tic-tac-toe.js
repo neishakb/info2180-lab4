abeaad9c04e1bf4dc9f5ae26c1dc211b2080635c
Neisha Brown <neishakbrown@gmail.com>
2019-10-17 10:07:09 -0500

    Added intital boxes

---
+++ tic-tac-toe.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+
+        let dBoard = document.getElementById("board").children;
+        let count = 1;
+    
+        for(n = 0; n < dBoard.length; n++){
+            dBoard[n].classList.add("square");
+    
+        }
+    
+
+} ;
