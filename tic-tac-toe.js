36dd19d6c516681beb141718a3f5a0a9ed0f9288
Neisha Brown <neishakbrown@gmail.com>
2019-10-17 18:15:39 -0500

    Addition of X's and O's

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,33 @@
 window.onload = () => {
 
-        let dBoard = document.getElementById("board").children;
-        let count = 1;
-    
-        for(n = 0; n < dBoard.length; n++){
-            dBoard[n].classList.add("square");
-    
-        }
-    
+     let dBoard = document.getElementById("board").children;
+     let response = "X";
 
-} ;
+     
+    
+    for(let n = 0; n < dBoard.length; n++){
+        dBoard[n].classList.add("square");
+        dBoard[n].addEventListener("click",() => {
+            if(dBoard[n].innerHTML === "X" || dBoard[n].innerHTML === "O"){
+                return;
+            }else{
+                if(response === "X"){
+                    dBoard[n].innerHTML = "X";
+                    dBoard[n].classList.add("X");
+                    response = "O";
+                }else{
+                    dBoard[n].innerHTML = "O"
+                    dBoard[n].classList.add("O");
+                    response = "X";
+                }
+           
+            }
+            
+        });
+
+
+    }
+   
+
+};
+
