23b7643e543512dd775560e7a1601819b0ac2fc0
Neisha Brown <neishakbrown@gmail.com>
2019-10-18 15:58:14 -0500

    Working Restart Button

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,6 +3,16 @@ window.onload = () => {
      let dBoard = document.getElementById("board").children;
      let response = "X";
      let divstatus = document.getElementById("status");
+     let button = document.getElementsByClassName("btn")[0];
+
+     button.addEventListener("click", () => {
+        for(let r = 0; r <dBoard.length; r++){
+            dBoard[r].innerHTML = " ";
+        }
+
+        divstatus.innerHTML = "Move your mouse over a square and click to play an X or an O.";
+        divstatus.classList.remove("you-won");
+     });
 
      
     
@@ -25,6 +35,8 @@ window.onload = () => {
                     if(possibleCombos("X")){
                         divstatus.innerHTML = "Congratulations! X is the Winner!";
                         divstatus.classList.add("you-won");
+
+                        
                     }
                 }else{
                     dBoard[n].innerHTML = "O"
@@ -33,6 +45,10 @@ window.onload = () => {
                     if(possibleCombos("O")){
                         divstatus.innerHTML = "Congratulations! O is the Winner!";
                         divstatus.classList.add("you-won");
+
+                        for( let p = 0; p < dBoard.length; p++){
+                            dBoard[p].addEventListener("click",() => {})
+                        }
                     }
                 }
            
@@ -43,30 +59,41 @@ window.onload = () => {
 
     }
 
+
+
     const possibleCombos = (letter) => {
         //checking columns for winning combos
        for(let n = 0; n < 3; n++){
            if((dBoard[n].innerHTML === letter) && (dBoard[n + 3].innerHTML === letter) && (dBoard[n + 6].innerHTML === letter)){
-               //do something
-               return true;
+            for( let k = 0; k < dBoard.length; k++){
+                dBoard[k].addEventListener("click",() => {})
+            }
+            return true;
            }
        } 
 
        //checking rows for winning combos
        for(let j = 0; j < 7; j+= 3){
             if((dBoard[j].innerHTML === letter) && (dBoard[j + 1].innerHTML === letter) && (dBoard[j + 2].innerHTML === letter)){
-                //do something
+                
+                for( let k = 0; k < dBoard.length; k++){
+                    dBoard[k].addEventListener("click",() => {})
+                }
                 return true;
             }
     
         }
 
         if(((dBoard[0].innerHTML === letter) && (dBoard[4].innerHTML === letter) && (dBoard[8].innerHTML === letter)) || ((dBoard[2].innerHTML === letter) && (dBoard[4].innerHTML === letter) && (dBoard[6].innerHTML === letter))){
+            for( let k = 0; k < dBoard.length; k++){
+                dBoard[k].addEventListener("click",() => {})
+            }
             return true;
 
         }
         return false;
     }
+    
 
 };
    
